2dd73c52c77e2235849331996a5344c786e900e4
Tanima-Sinha <sinhatanima9@gmail.com>
2025-05-12 15:27:24 +0530

    Update restaurantbook.aspx.vb

--- WebApplication1/Resturant/resturantbook.aspx.vb
+++ WebApplication1/Resturant/resturantbook.aspx.vb
@@ -4,64 +4,50 @@ Imports System.Data.SqlClient
 Public Class resturantbook
     Inherits System.Web.UI.Page
 
-    Dim con As New SqlConnection("Data Source=DESKTOP-09AJEAD\SQLEXPRESS;Initial Catalog=major;Integrated Security=True;Pooling=False")
-
+    Dim con As New SqlConnection("Data Source=DESKTOP-JPPAMD6\SQLEXPRESS01;Initial Catalog=major;Integrated Security=True;Pooling=False")
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Not IsPostBack Then
             con.Open()
-            Dim cmd As New SqlCommand("SELECT TOP 1 price FROM resturant", con)
+            Dim cmd As New SqlCommand("SELECT TOP 1 price FROM restaurant", con)
             Dim price As Object = cmd.ExecuteScalar()
+            con.Close()
 
             If price IsNot Nothing Then
                 TextBox2.Text = price.ToString()
             End If
-
-
         End If
-
     End Sub
 
     Protected Sub btnbookrest_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnbookrest.Click
-
-        If Session("loggedInUser") Is Nothing Then
-            Response.Redirect("Custlogin.aspx")
-        Else
-            Dim type As String = TextBox1.Text
-            Dim price As Integer = Convert.ToInt32(TextBox2.Text)
-
-            con.Open()
-            Dim cmd As New SqlCommand("INSERT INTO resturant (type, price, status) VALUES (@type, @price, 'pending')", con)
-            cmd.Parameters.AddWithValue("@type", type)
-            cmd.Parameters.AddWithValue("@price", price)
-            cmd.ExecuteNonQuery()
-            con.Close()
-
-            ' Store price in session for later update
-            Session("price") = price
-            Response.Redirect("bookingrest_confirmation.aspx")
-        End If
+        HandleBooking(TextBox1, TextBox2)
     End Sub
 
+    Protected Sub btnbookrest2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnbookrest2.Click
+        HandleBooking(TextBox3, TextBox4)
+    End Sub
 
-
-    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnbookrest2.Click
+    Private Sub HandleBooking(ByVal typeBox As TextBox, ByVal priceBox As TextBox)
         If Session("loggedInUser") Is Nothing Then
             Response.Redirect("Custlogin.aspx")
-        Else
-            Dim type As String = TextBox3.Text
-            Dim price As Integer = Convert.ToInt32(TextBox4.Text)
-
-            con.Open()
-            Dim cmd As New SqlCommand("INSERT INTO resturant (type, price, status) VALUES (@type, @price, 'pending')", con)
-            cmd.Parameters.AddWithValue("@type", type)
-            cmd.Parameters.AddWithValue("@price", price)
-            cmd.ExecuteNonQuery()
-            con.Close()
-
-            ' Store price in session for later update
-            Session("price") = price
-            Response.Redirect("bookingrest_confirmation.aspx")
+            Exit Sub
         End If
+
+        Dim type As String = typeBox.Text
+        Dim price As Integer
+        If Not Integer.TryParse(priceBox.Text, price) Then
+            MsgBox("Invalid price.")
+            Exit Sub
+        End If
+
+        ' Store data in session
+        Dim bookings As New List(Of Tuple(Of String, Integer))
+        If Session("pendingBookings") IsNot Nothing Then
+            bookings = CType(Session("pendingBookings"), List(Of Tuple(Of String, Integer)))
+        End If
+        bookings.Add(Tuple.Create(type, price))
+        Session("pendingBookings") = bookings
+
+        Response.Redirect("bookingrest_confirmation.aspx")
     End Sub
 End Class
